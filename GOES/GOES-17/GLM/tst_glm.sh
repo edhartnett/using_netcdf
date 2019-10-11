223d5c07f68a5578e16525e1e6b46b79a9cf33f5
Ed <edwardjameshartnett@gmail.com>
2019-10-11 02:39:30 -0600

    fixed wget command

--- GOES/GOES-17/GLM/tst_glm.sh
+++ GOES/GOES-17/GLM/tst_glm.sh
@@ -4,11 +4,12 @@ set -e
 # Get files if needed.
 echo ""
 file_list="OR_GLM-L2-LCFA_G17_s20192692359400_e20192700000000_c20192700000028.nc"
-echo "Getting GLM test file $file_list"
+echo "Checking for GLM test file $file_list"
 for f1 in $file_list
 do
     if ! test -f $f1; then
-	wget https://github.com/edhartnett/data_using_netcdf/blob/master/GOES/GOES-17/GLM$f1
+	echo "Getting GLM test file $f1"
+	wget https://github.com/edhartnett/data_using_netcdf/blob/master/GOES/GOES-17/GLM/$f1
     fi
 done
 

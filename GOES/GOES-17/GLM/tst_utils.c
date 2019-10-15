a3e6ed7b400c92e4a6e6a3fba52fd352d93d206c
Ed Hartnett <edwardjameshartnett@gmail.com>
2019-10-15 08:04:40 -0600

    starting to add timing

--- GOES/GOES-17/GLM/tst_utils.c
+++ GOES/GOES-17/GLM/tst_utils.c
@@ -3,36 +3,36 @@
  * Ed Hartnett
  * 10/15/19
  * Amsterdam
-*/
+ */
 
 #include <time.h>
 #include <sys/time.h>
 #include "un_test.h"
 
 /** Subtract the `struct timeval' values X and Y, storing the result in
-   RESULT.  Return 1 if the difference is negative, otherwise 0.  This
-   function from the GNU documentation. */
+    RESULT.  Return 1 if the difference is negative, otherwise 0.  This
+    function from the GNU documentation. */
 int
-nc4_timeval_subtract (result, x, y)
-   struct timeval *result, *x, *y;
+un_timeval_subtract (result, x, y)
+    struct timeval *result, *x, *y;
 {
-   /* Perform the carry for the later subtraction by updating Y. */
-   if (x->tv_usec < y->tv_usec) {
-      int nsec = (y->tv_usec - x->tv_usec) / MILLION + 1;
-      y->tv_usec -= MILLION * nsec;
-      y->tv_sec += nsec;
-   }
-   if (x->tv_usec - y->tv_usec > MILLION) {
-      int nsec = (x->tv_usec - y->tv_usec) / MILLION;
-      y->tv_usec += MILLION * nsec;
-      y->tv_sec -= nsec;
-   }
+    /* Perform the carry for the later subtraction by updating Y. */
+    if (x->tv_usec < y->tv_usec) {
+        int nsec = (y->tv_usec - x->tv_usec) / MILLION + 1;
+        y->tv_usec -= MILLION * nsec;
+        y->tv_sec += nsec;
+    }
+    if (x->tv_usec - y->tv_usec > MILLION) {
+        int nsec = (x->tv_usec - y->tv_usec) / MILLION;
+        y->tv_usec += MILLION * nsec;
+        y->tv_sec -= nsec;
+    }
 
-   /* Compute the time remaining to wait.
-      `tv_usec' is certainly positive. */
-   result->tv_sec = x->tv_sec - y->tv_sec;
-   result->tv_usec = x->tv_usec - y->tv_usec;
+    /* Compute the time remaining to wait.
+       `tv_usec' is certainly positive. */
+    result->tv_sec = x->tv_sec - y->tv_sec;
+    result->tv_usec = x->tv_usec - y->tv_usec;
 
-   /* Return 1 if result is negative. */
-   return x->tv_sec < y->tv_sec;
+    /* Return 1 if result is negative. */
+    return x->tv_sec < y->tv_sec;
 }

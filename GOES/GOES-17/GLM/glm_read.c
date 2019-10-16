59b54cb5ab60767be16bbe56d623e1019e6fc4e4
Ed Hartnett <edwardjameshartnett@gmail.com>
2019-10-16 04:11:00 -0600

    added timing

--- GOES/GOES-17/GLM/glm_read.c
+++ GOES/GOES-17/GLM/glm_read.c
@@ -24,10 +24,15 @@
 /* Number of timing runs. */
 #define NUM_TRIALS 10
 
+/* These are dimension names in the GLM data file. */
+#define NUMBER_OF_FLASHES "number_of_flashes"
+#define NUMBER_OF_GROUPS "number_of_groups"
+#define NUMBER_OF_EVENTS "number_of_events"
+
 /* Usage description. */
 #define USAGE   "\
   [-v]        Verbose\n\
-  [-h]        Print output header\n"
+  [-t]        Perform timing runs\n"
 
 /* Read a text attribute and print its value. */
 int
@@ -61,22 +66,74 @@ show_att(int ncid, int varid, char *name)
 }
 
 int
-main(int argc, char **argv)
+glm_read_file(char *file_name, int verbose)
 {
     int ncid;
+    size_t nevents, ngroups, nflashes;
+    int event_dimid, group_dimid, flash_dimid;
+    int ret;
+    
+    /* Open the data file as read-only. */
+    if ((ret = nc_open(file_name, NC_NOWRITE, &ncid)))
+	NC_ERR(ret);
+
+    /* Optionally display some of the global attributes. The GLM data
+     * files comply with the CF Conventions, and other metadata
+     * standards. */
+    if (verbose)
+    {
+	if (show_att(ncid, NC_GLOBAL, TITLE))
+	    ERR;
+	if (show_att(ncid, NC_GLOBAL, PLATFORM_ID))
+	    ERR;
+	if (show_att(ncid, NC_GLOBAL, SUMMARY))
+	    ERR;
+    }
+
+    /* Read the size of the dimensions. */
+    if ((ret = nc_inq_dimid(ncid, NUMBER_OF_FLASHES, &flash_dimid)))
+	NC_ERR(ret);
+    if ((ret = nc_inq_dimlen(ncid, flash_dimid, &nflashes)))
+	NC_ERR(ret);
+    
+    if ((ret = nc_inq_dimid(ncid, NUMBER_OF_GROUPS, &group_dimid)))
+	NC_ERR(ret);
+    if ((ret = nc_inq_dimlen(ncid, group_dimid, &ngroups)))
+	NC_ERR(ret);
+
+    if ((ret = nc_inq_dimid(ncid, NUMBER_OF_EVENTS, &event_dimid)))
+	NC_ERR(ret);
+    if ((ret = nc_inq_dimlen(ncid, event_dimid, &nevents)))
+	NC_ERR(ret);
+
+    /* Close the data file. */
+    if ((ret = nc_close(ncid)))
+	NC_ERR(ret);
+    
+    return 0;
+}
+
+int
+main(int argc, char **argv)
+{
     int c;
     int verbose = 0;
+    int timing = 0;
     struct timeval start_time, end_time, diff_time;
     int meta_read_us = 0, meta_max_read_us = 0, meta_min_read_us = NC_MAX_INT;
     int meta_tot_read_us = 0, meta_avg_read_us;
+    int num_trials = 1;
     int t;
-    int ret;
     
-    while ((c = getopt(argc, argv, "v")) != EOF)
+    while ((c = getopt(argc, argv, "vt")) != EOF)
 	switch(c)
 	{
 	case 'v':
 	    verbose++;
+	    break;
+	case 't':
+	    timing++;
+	    num_trials = NUM_TRIALS;
 	    break;
 	case '?':
 	    fprintf(stderr, "glm_read -v -h\n%s", USAGE);
@@ -86,22 +143,18 @@ main(int argc, char **argv)
     if (verbose)
 	printf("Reading Geostationary Lightning Mapper data\n");
 
-    for (t = 0; t < NUM_TRIALS; t++)
+    for (t = 0; t < num_trials; t++)
     {
+	/* Clear all buffers. */
+	if (timing)
+	    system("./clearcache.sh");
+
+	/* Start timer. */
 	if (gettimeofday(&start_time, NULL))
 	    ERR;
-    
-	/* Open the data file as read-only. */
-	if ((ret = nc_open(GLM_DATA_FILE, NC_NOWRITE, &ncid)))
-	    NC_ERR(ret);
 
-	/* Read some of the global attributes. The GLM data files comply
-	 * with the CF Conventions, and other metadata standards. */
-	if (show_att(ncid, NC_GLOBAL, TITLE))
-	    ERR;
-	if (show_att(ncid, NC_GLOBAL, PLATFORM_ID))
-	    ERR;
-	if (show_att(ncid, NC_GLOBAL, SUMMARY))
+	/* Read file. */
+	if (glm_read_file(GLM_DATA_FILE, verbose))
 	    ERR;
 
 	/* Handle timing. */
@@ -113,16 +166,14 @@ main(int argc, char **argv)
 	if (meta_read_us < meta_min_read_us)
 	    meta_min_read_us = meta_read_us;
 	meta_tot_read_us += meta_read_us;
-
-	/* Close the data file. */
-	if ((ret = nc_close(ncid)))
-	    NC_ERR(ret);
     }
-    meta_avg_read_us = meta_tot_read_us / NUM_TRIALS;
-    if (verbose)
+
+    if (timing)
     {
+	meta_avg_read_us = meta_tot_read_us / NUM_TRIALS;
 	printf("meta_avg_read_us \t meta_min_read_us \t meta_max_read_us\n");
-	printf("%d\t%d\t%d\n", meta_avg_read_us, meta_min_read_us, meta_max_read_us);
+	printf("%d\t\t\t\t%d\t\t\t\t%d\n", meta_avg_read_us, meta_min_read_us,
+	       meta_max_read_us);
     }
     
     if (verbose)
